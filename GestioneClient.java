bac56665400105d9045f20a64de27017b7302741
Francesco Orlando <93184417+fra81norlando@users.noreply.github.com>
2022-04-28 10:12:59 +0200

    Update GestioneClient.java

--- GestioneClient.java
+++ GestioneClient.java
@@ -1,21 +1,19 @@
 
-package cscountdown;
-
+import java.io.IOException;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 
 public class GestioneClient {
-    public static void main(String[] args) {
-        try {
-            Client cli = new Client(InetAddress.getLocalHost(), 2000);
-            String msgLetto = cli.leggi();
-            System.out.println(msgLetto);
-        } catch (UnknownHostException ex) {
-            Logger.getLogger(GestioneClient.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-    
+
+	public static void main(String[] args) {
+		try{
+	        Client cli = new Client(InetAddress.getLocalHost(), 2000);
+	        System.out.println("Client connesso");
+	        System.out.println("Server: " +cli.leggi());
+	        Countdown cd = new Countdown(Integer.getInteger(cli.leggi()));
+	        cd.start();
+	        }
+		catch(IOException e){
+	            e.printStackTrace();
+	        }
+	}
 }
