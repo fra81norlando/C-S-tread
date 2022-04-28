e0f72010a459fd7ea31695f91d0d059bf277e2a0
Francesco Orlando <93184417+fra81norlando@users.noreply.github.com>
2022-04-28 10:28:13 +0200

    Update GestioneClient.java

--- GestioneClient.java
+++ GestioneClient.java
@@ -7,7 +7,7 @@ public class GestioneClient {
 	public static void main(String[] args) {
 		try{
 	        Client cli = new Client(InetAddress.getLocalHost(), 2000);
-	        System.out.println("Client connesso");
+	        System.out.println("Client connesso");// connessione con il client avvenuta
 	        System.out.println("Server: " +cli.leggi());
 	        Countdown cd = new Countdown(Integer.getInteger(cli.leggi()));
 	        cd.start();
