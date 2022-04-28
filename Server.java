41088f54c4dd280bbeb1c60b4d5e4e729f44f2fe
Francesco Orlando <93184417+fra81norlando@users.noreply.github.com>
2022-04-28 10:15:55 +0200

    Create Server.java

---
+++ Server.java
@@ -0,0 +1,65 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.BindException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+
+public class Server {
+	ServerSocket ss;
+    Socket so;
+    DataOutputStream out;
+    DataInputStream in;
+    
+    public Server(int porta){
+        try{
+            ss = new ServerSocket(porta);
+            System.out.println();
+            Countdown cd = new Countdown(5);
+    		cd.start();
+            ss.setSoTimeout(5000);
+        }
+        catch(SocketTimeoutException e) {
+        	e.printStackTrace();
+        }
+        catch(BindException e) {
+			System.err.println("porta occupata");
+			e.printStackTrace();
+		}
+        catch (IOException ex){
+        	ex.printStackTrace();
+        }
+        
+    }
+    public void inAscolto(){
+        try{
+	        so = ss.accept();
+	        System.out.println("connessione stabilita");
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+    public void scrivi(String messaggio){
+        try {
+        	out = new DataOutputStream(so.getOutputStream());
+        	out.writeUTF(messaggio);
+        }
+        catch(IOException e) {
+        	e.printStackTrace();
+        }
+        
+    }
+    public String leggi(){
+    	String msg = "";
+        try{
+        	in = new DataInputStream(so.getInputStream());
+        	msg = in.readUTF();
+        }
+        catch(IOException e) {
+        	e.printStackTrace();
+        }
+        return msg;
+    }
+}
